34c7a7c4e15143cba1bda89900b0890eaf132b81
kd.shi <kd.shi@slancer.com>
2018-09-25 13:58:04 +0800

    add config for nexuse and nginx proxy for nexuse

--- docs/deploy.md
+++ docs/deploy.md
@@ -3,3 +3,9 @@ init deploy node
 ```bash
 ./tools/deploy.sh
 ```
+
+# setup repo
+setup docker yum repo on nexus
+
+# upload package
+upload rpm packages and docker images
--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,2 +1,21 @@
-PROJECT=`pwd`
-bash $PROJECT/boot/apt/sit.sh
+#!/usr/bin/env bash
+
+function init_env{
+    export PROJECT=`pwd`
+}
+
+function up_source_repo{
+    docker-compose -f $PROJECT/boot/docker-compose.yml start
+}
+
+function init_node{
+    bash $PROJECT/boot/apt/sit.sh
+}
+
+function main{
+    init_env
+    init_node
+    up_source_repo
+}
+
+main
