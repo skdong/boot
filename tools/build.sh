99107325c3e58b01f6da1d046108f6e34786f83e
kd.shi <kd.shi@slancer.com>
2018-11-07 11:51:37 +0800

    add ceph openstack deb packages

--- tools/build.sh
+++ tools/build.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 MODULES="ansible apt pypi rpm docker"
+MODULE=$(dirname $(readlink -f $0))
+BOOT=$MODULE/../boot
 
 function build_docker_image() {
     for module in $MODULES:
     do
-        if [ -f ../boot/$module/build.sh ] ; then
-            bash ../boot/$module/build.sh
+        if [ -f $BOOT/$module/build.sh ] ; then
+            echo "buid $module image"
+            bash $BOOT/$module/build.sh
         fi
     done
 }
@@ -13,8 +16,9 @@ function build_docker_image() {
 function build_packages() {
     for module in $MODULES:
     do
-        if [ -f ../boot/$module/packages/build.sh ] ; then
-            bash ../boot/$module/packages/build.sh
+        if [ -f $BOOT/$module/packages/build.sh ] ; then
+            echo "buid $module pakcages"
+            bash $BOOT/$module/packages/build.sh
         fi
     done
 }
