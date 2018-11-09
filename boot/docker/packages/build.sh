d8baa7f93207b7fe3088d7e7840fe0416da307ee
kd.shi <kd.shi@slancer.com>
2018-11-09 11:44:29 +0800

    tran docker images use /var/lib/docker add root permission

--- boot/docker/packages/build.sh
+++ boot/docker/packages/build.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 MODULE=$(dirname $(readlink -f $0))
 name="dire_docker_builder"
 
 function building_packages(){
-    docker run  -d --rm -v /opt/dire/packages/docker:/opt/dire/packages/docker \
+    docker run  -d --rm --privileged \
+     -v /opt/dire/packages/docker:/opt/dire/packages/docker \
      -v $MODULE/download.sh:/usr/bin/download.sh \
      -v $MODULE/images.d:/opt/dire/docker/images.d \
      -v /etc/docker/certs.d/:/etc/docker/certs.d/ \
      -v /etc/hosts:/etc/hosts \
      --name $name docker:dind
     docker exec -it -uroot $name /bin/sh /usr/bin/download.sh
-    docker rm -f $name
+    docker stop $name
  }
 
 docker inspect $name > /dev/null 2>&1
