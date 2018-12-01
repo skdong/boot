d6b4317825b5d1c81b62e79193f15b5ee346e61b
kaidong <shikd@bjzdgt.com>
2018-12-01 15:46:24 +0800

    add tools param

--- tools/ansible_container.sh
+++ tools/ansible_container.sh
@@ -1,7 +1,11 @@
 #/usr/bin/env bash
 
-docker rm -f ansible_tool
-docker run -it -d --name ansible_tool --hostname dire_tool \
+ansible_tool=$1
+ansible_tool=${ansible_tool:='ansible_tool'}
+echo $ansible_tool
+
+#docker rm  ansible_tool
+docker run -it -d --name $ansible_tool --hostname $ansible_tool \
            -v /home/kolla/.ssh/:/root/.ssh \
            -v /home/kolla/dire:/opt/dire dire/ansible_tools bash
 docker start ansible_tool
