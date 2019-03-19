b4e477451aa59622919d39867daa69901d2ef641
中林 张 <zhangzl@bjzdgt.com>
2019-03-19 10:42:46 +0800

    Update create_runner.sh

--- tools/create_runner.sh
+++ tools/create_runner.sh
@@ -3,16 +3,22 @@
 MODULE=$(dirname $(readlink -f $0))
 source $MODULE/bootrc
 
-docker run --rm -t -i -v /srv/gitlab-runner/config:/etc/gitlab-runner gitlab/gitlab-runner register \
-  --non-interactive \
-  --executor "docker" \
-  --docker-image dire/deploy \
-  --docker-privileged=false \
-  --docker-pull-policy="if-not-present" \
-  --docker-extra-hosts "gitlab$DOMAIN:$GIT_HOST,$HOST_NAME:$HOST" \
+docker run -d --name gitlab-runner --restart always \
+  -v /srv/gitlab-runner/config:/etc/gitlab-runner \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  gitlab/gitlab-runner:latest
+  
+docker exec -it gitlab-runner gitlab-ci-multi-runner register -n \
   --url http://$GIT_HOST/ \
   --registration-token "$GIT_RUNNER_TOKEN" \
+  --tag-list=dire \
   --description "dire" \
-  --tag-list "dire" \
+  --docker-privileged=false \
+  --docker-pull-policy="if-not-present" \
+  --docker-image "dire/deploy" \
+  --docker-volumes "/cache" \
+  --docker-extra-hosts "gitlab$DOMAIN:$GIT_HOST" \
+  --docker-extra-hosts "$HOST_NAME:$HOST" \
+  --executor docker \
   --run-untagged \
   --locked="false"
