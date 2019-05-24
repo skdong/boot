f2e58342bce38eb441f54a3cc1545083f33e278f
kd.shi <kd.shi@slancer.com>
2019-05-24 10:03:52 +0800

    add command args verify

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -9,10 +9,10 @@ source $MODULE/bootrc
 ENABLE_HELM=${ENABLE_HELM:-'no'}
 ENABLE_GITLAB=${ENABLE_GITLAB:-'no'}
 
-function uncompress_project() {
+function decompress_project() {
     if [[ -f $1.tar.gz ]] && [[ ! -d $1 ]] ; then
         tar -zxf $1.tar.gz
-        echo "uncompress $1 over"
+        echo "decompress $1 over"
     else
         if [[ ! -d $1 ]] ; then
             echo "need $1 packages file"
@@ -22,30 +22,79 @@ function uncompress_project() {
     fi
 }
 
-function uncompress() {
-    echo "uncompress packages"
+# decompress packages
+# deb pypi rpm
+# docker package decompress when install docker
+function decompress() {
+    echo "decompress packages"
     cd /opt/dire/packages
-    uncompress_project deb
-    uncompress_project pypi
-    uncompress_project rpm
+
+    for project in deb pypi rpm
+    do
+        decompress_project $project
+    done
+
     if [[ $ENABLE_HELM == 'yes' ]] ; then
-        uncompress_project helm
+        decompress_project helm
     fi
 }
-function init_node() {
+
+# init deploy node env
+# set local repo
+# install common package
+# install docker-ce
+# load docker images
+function initiate() {
+    echo "init depoly node"
     bash $PROJECT/boot/apt/sit.sh
     echo "init over"
 }
 
-function up_servers() {
+function serve() {
     bash $PROJECT/boot/servers/sit.sh
     echo "up servers over"
 }
 
-function main() {
-    uncompress
-    init_node
-    up_servers
+function all() {
+    decompress
+    initiate
+    serve
 }
 
-main
+function usage() {
+    echo "################################################"
+    echo "# decompress|initiate|serve|all"
+    echo "# decompress: decompress pypi deb rpm package"
+    echo "# initiate: ini deploy node"
+    echo "# serve: up neuxs and gitlab server"
+    echo "# all: do up action all"
+    echo "################################################"
+}
+
+if [ $# == 0 ]; then
+    all
+fi
+
+if [ $# -gt 1 ]; then
+    usage
+fi
+
+case "$1" in
+    decompress)
+        decompress
+        ::
+    initiate)
+        initiate
+        ;;
+    serve)
+        serve
+        ;;
+    all)
+        all
+        ;;
+    *)
+        usage
+        exit 1
+esac
+
+exit 0
